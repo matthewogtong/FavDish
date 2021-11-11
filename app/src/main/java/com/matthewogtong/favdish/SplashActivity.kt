db7fc03ca9739f1dc9b99242bb7f3730eeeedd9f
matthewogtong <ogtongm@gmail.com>
2021-11-10 21:45:31 -0500

    Remove action bar and enable full screen for splash screen

--- app/src/main/java/com/matthewogtong/favdish/SplashActivity.kt
+++ app/src/main/java/com/matthewogtong/favdish/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.matthewogtong.favdish
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.matthewogtong.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -12,5 +15,14 @@ class SplashActivity : AppCompatActivity() {
         val splashBinding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
         setContentView(splashBinding.root)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 }
