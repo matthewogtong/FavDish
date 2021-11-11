f603f45a37bde5003e27a228893f8c46c9b14a7e
matthewogtong <ogtongm@gmail.com>
2021-11-10 21:34:18 -0500

    Add Splash Activity to Manifest and establish binding

--- app/src/main/java/com/matthewogtong/favdish/SplashActivity.kt
+++ app/src/main/java/com/matthewogtong/favdish/SplashActivity.kt
@@ -2,10 +2,15 @@ package com.matthewogtong.favdish
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.matthewogtong.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val splashBinding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
+        setContentView(splashBinding.root)
+
     }
 }
