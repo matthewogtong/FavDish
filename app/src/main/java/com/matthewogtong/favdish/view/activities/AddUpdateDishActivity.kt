99a3331e6aab250e833535dc0c832a0d6858dbbf
matthewogtong <ogtongm@gmail.com>
2021-11-16 21:08:16 -0500

    Add Camera and Storage permission

--- app/src/main/java/com/matthewogtong/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/matthewogtong/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,10 +1,21 @@
 package com.matthewogtong.favdish.view.activities
 
+import android.Manifest
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.matthewogtong.favdish.R
 import com.matthewogtong.favdish.databinding.ActivityAddUpdateDishBinding
 import com.matthewogtong.favdish.databinding.DialogCustomImageSelectionBinding
@@ -72,16 +83,94 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
         // Assign the click for Camera and Gallery. Show the Toast message for now.
         binding.tvCamera.setOnClickListener {
-            Toast.makeText(this@AddUpdateDishActivity, "You have clicked on the Camera.", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this@AddUpdateDishActivity)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        // Here after all the permission are granted launch the CAMERA to capture an image.
+                        if (report!!.areAllPermissionsGranted()) {
+
+                            // Step 4: Show the Toast message for now just to know that we have the permission.
+                            Toast.makeText(
+                                this@AddUpdateDishActivity,
+                                "You have the Camera permission now to capture image.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        // 6: Show the alert dialog
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
+
             dialog.dismiss()
         }
 
         binding.tvGallery.setOnClickListener {
-            Toast.makeText(this@AddUpdateDishActivity, "You have clicked on the Gallery.", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this@AddUpdateDishActivity)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+
+                        // Here after all the permission are granted launch the gallery to select and image.
+                        if (report!!.areAllPermissionsGranted()) {
+                            // 8: Show the Toast message for now just to know that we have the permission.
+                            Toast.makeText(
+                                this@AddUpdateDishActivity,
+                                "You have the Gallery permission now to select image.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
             dialog.dismiss()
         }
 
         //Start the dialog and display it on screen.
         dialog.show()
+    }
+
+    /**
+     * A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
+     */
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 }
