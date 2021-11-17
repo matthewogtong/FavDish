987548374766d6a85e5eb2767e06bcd1a96ec208
matthewogtong <ogtongm@gmail.com>
2021-11-16 20:54:43 -0500

    Implement the custom Dialog for Camera and Gallery selection

--- app/src/main/java/com/matthewogtong/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/matthewogtong/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,17 @@
 package com.matthewogtong.favdish.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.matthewogtong.favdish.R
 import com.matthewogtong.favdish.databinding.ActivityAddUpdateDishBinding
+import com.matthewogtong.favdish.databinding.DialogCustomImageSelectionBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+// Implement the View.OnClickListener
+
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     // Create a global variable for layout ViewBinding
     private lateinit var mBinding: ActivityAddUpdateDishBinding
@@ -19,6 +25,24 @@ class AddUpdateDishActivity : AppCompatActivity() {
 
         // Call the method of setupActionBar
         setupActionBar()
+
+        // Assign the click event to the image button.
+        mBinding.ivAddDishImage.setOnClickListener(this@AddUpdateDishActivity)
+    }
+
+    // Override the onclick listener method.
+    override fun onClick(v: View) {
+
+        // Perform the action when user clicks on the addDishImage and show Toast message for now.
+        when (v.id) {
+
+            R.id.iv_add_dish_image -> {
+
+                customImageSelectionDialog()
+
+                return
+            }
+        }
     }
 
     // Create a function to setup the ActionBar
@@ -27,6 +51,37 @@ class AddUpdateDishActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        // Replace the back button that we have generated.
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+
         mBinding.toolbarAddDishActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    // Create a function to launch the custom dialog.
+    /**
+     * A function to launch the custom image selection dialog.
+     */
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this@AddUpdateDishActivity)
+
+        val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+
+        /*Set the screen content from a layout resource.
+        The resource will be inflated, adding all top-level views to the screen.*/
+        dialog.setContentView(binding.root)
+
+        // Assign the click for Camera and Gallery. Show the Toast message for now.
+        binding.tvCamera.setOnClickListener {
+            Toast.makeText(this@AddUpdateDishActivity, "You have clicked on the Camera.", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        binding.tvGallery.setOnClickListener {
+            Toast.makeText(this@AddUpdateDishActivity, "You have clicked on the Gallery.", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        //Start the dialog and display it on screen.
+        dialog.show()
     }
 }
