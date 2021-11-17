d785c338e979c1b9f5c19d727aeb921a8be684be
matthewogtong <ogtongm@gmail.com>
2021-11-16 20:16:55 -0500

    Designing AddUpdateAcitivity part 2

--- app/src/main/java/com/matthewogtong/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/matthewogtong/favdish/view/activities/AddUpdateDishActivity.kt
@@ -3,10 +3,30 @@ package com.matthewogtong.favdish.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.matthewogtong.favdish.R
+import com.matthewogtong.favdish.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+
+    // Create a global variable for layout ViewBinding
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+
+        // Initialize the layout ViewBinding variable and set the contentView.
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        // Call the method of setupActionBar
+        setupActionBar()
+    }
+
+    // Create a function to setup the ActionBar
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener { onBackPressed() }
     }
 }
